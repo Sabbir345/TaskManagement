c9e102ad90d0cf60d7d524c12fd5a45b42e13607
sabbir345 <sabbir.ahmad111@gmail.comgit config --global user.email sabbir.ahmad111@gmail.comgit config --global user.email sabbir.ahmad111@gmail.com>
2019-11-01 02:15:51 +0600

    delete some Unnecessary line

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@
     // For User Routes
 	
     Auth::routes();
-    // Route::get('/' , 'User\UserController@index')->name('user.login');
-    // Route::post('/' , 'User\UserController@Login')->name('user.login.confirm');
 
     Route::group([
         'middleware' => 'auth'
@@ -37,18 +35,3 @@
       include_once 'Admin/dashboard.php';
 
     });
-
-        Route::get('task-created', function(){
-            \App\Task::create(['name'=>'test 2', 'price'=>100]);
-            dd('Task created successfully.');
-        });
-
-        Route::get('task-created-updated', function(){
-            \App\Task::find(1)->update(['name'=>'test 3', 'price'=>100]);
-            dd('Task updated successfully.');
-        });
-
-        Route::get('task-deleted', function(){
-            \App\Task::find(1)->delete();
-            dd('Task deleted successfully.');
-        });
